7348934d2aa9a807be6ea95d03cb22d94fe64258
Kirill Kapustin <kanyct04ka@yandex.ru>
2025-12-02 13:54:04 +0300

    fixed: date validation

--- stats/stats-server/src/main/java/ru/practicum/ewm/stats/exception/ExceptionErrorHandler.java
+++ stats/stats-server/src/main/java/ru/practicum/ewm/stats/exception/ExceptionErrorHandler.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
@@ -14,6 +15,15 @@ import java.util.Map;
 @Slf4j
 @RestControllerAdvice
 public class ExceptionErrorHandler {
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public ResponseEntity<Object> handleMissingServletRequestParameter(MissingServletRequestParameterException e) {
+        log.warn("Ошибка недостаточности параметров: {}", e.getMessage());
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(Map.of("error", e.getMessage()));
+    }
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<Object> handleIllegalArgument(IllegalArgumentException e) {
